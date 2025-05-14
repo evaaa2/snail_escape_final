8666bb68c2723a2682d65f29daf8b1e4307cdaa1
Eva Dekastello <eva.dekastello@email.cz>
2025-05-14 21:52:11 +0200

    renaming

---
+++ Snail/Assets/Scripts/SceneController.cs
@@ -0,0 +1,21 @@
+/*using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField]
+    private floaat _sceneFadeDuration;
+
+    private SceneFade _sceneFade;
+
+    private void Awake()
+    {
+        _sceneFade = GetComponentInChildren<SceneFade>();
+    }
+
+    private IEnumerator Start()
+    {
+        yield return _sceneFade.FadeInCoroutine(_sceneFadeDuration);
+    }
+}
+}
+*/
