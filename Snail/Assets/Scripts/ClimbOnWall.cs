a214c1bc7ddcb0fc39ee4eb2b7a970e7d0aabc52
tereza <terezaste178@gmail.com>
2025-05-15 21:32:45 +0200

    15. 5.

---
+++ Snail/Assets/Scripts/ClimbOnWall.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClimbOnWall : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    
+}
