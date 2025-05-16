b8af6c93648dad18f802464c16dd7d8f3db2ad35
tereza <terezaste178@gmail.com>
2025-05-16 07:05:40 +0200

    16. 5.

--- Snail/Assets/Scripts/ClimbOnWall.cs
+++ Snail/Assets/Scripts/ClimbOnWall.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class ClimbOnWall : MonoBehaviour
 {
-    public float speed = 5f;
+    /*public float speed = 5f;
 
     void Start()
     {
@@ -12,8 +12,16 @@ public class ClimbOnWall : MonoBehaviour
 
     void Update()
     {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        //the snail should not jump
+        moveY = 0;
 
-    }
+        if (moveX == Input.GetAxis("Horizontal") && RaycastHit2D)
+        {
+
+        }
+    }*/
 
     
 }
