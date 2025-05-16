6886d75ecbb9541d5b7831e790ac952321f38f45
tereza <terezaste178@gmail.com>
2025-05-16 08:40:02 +0200

    progress

--- Snail/Assets/Scripts/Player.cs
+++ Snail/Assets/Scripts/Player.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //public variables
+    public Rigidbody2D myRigidBody;
+    float horizontalMove = 0f;
+    private Vector2 movementDirection;
+    public Animator animator;
     public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,7 +20,29 @@ public class Player : MonoBehaviour
         float moveY = Input.GetAxis("Vertical");
         //the snail should not jump
         moveY = 0;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         transform.position += new Vector3(moveX, moveY, 0f) * speed * Time.deltaTime;
+
+        if (movementDirection.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (movementDirection.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow) || UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetBool("KeyDown", true);
+        }
+        else if (UnityEngine.Input.GetKeyUp(KeyCode.RightArrow) || UnityEngine.Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetBool("KeyDown", false);
+        }
+    }
+    void FixedUpdate()
+    {
+        myRigidBody.linearVelocity = movementDirection * speed;
     }
 }
