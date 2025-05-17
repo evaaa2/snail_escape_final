ccefcaaabf4c1f6a2cf862cd23d5157d905169a7
kzvolska <zvolskam@centrum.cz>
2025-05-17 21:17:43 +0200

    animace+nepada dolu, ale neprileha ke stenam a stropu a porad s nim jde pohybovat nahoru a dolu

--- Snail/Assets/Scripts/SnailScript.cs
+++ Snail/Assets/Scripts/SnailScript.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.Windows;
 
 public class SnailScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
+    public SpriteRenderer mySpriteRenderer;
     private float movementSpeed = 10f;
     float horizontalMove = 0f;
     private Vector2 movementDirection;
     public Animator animator;
+    public bool isOnCeiling = false;
+    public float directionMultiplier;
+    public bool isOnWall = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,19 +22,32 @@ public class SnailScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {      
-        horizontalMove = UnityEngine.Input.GetAxisRaw("Horizontal")* movementSpeed;
-        movementDirection = new Vector2(UnityEngine.Input.GetAxis("Horizontal"), UnityEngine.Input.GetAxis("Vertical"));
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        float inputX = UnityEngine.Input.GetAxisRaw("Horizontal");
+        float inputY = UnityEngine.Input.GetAxisRaw("Vertical");
+        horizontalMove = UnityEngine.Input.GetAxisRaw("Horizontal") * movementSpeed;
+        movementDirection = new Vector2(UnityEngine.Input.GetAxisRaw("Horizontal"), UnityEngine.Input.GetAxisRaw("Vertical"));
+        if (isOnWall)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        }
+        else 
+        {
+            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        }
+
 
         if (movementDirection.x > 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(directionMultiplier * -1, 1, 1);
+
         }
-        else if (movementDirection.x<0)
+        else if (movementDirection.x < 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(directionMultiplier * 1, 1, 1);
         }
+
         if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow) || UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow))
         {
             animator.SetBool("KeyDown", true);
@@ -40,7 +59,7 @@ public class SnailScript : MonoBehaviour
     }
     void FixedUpdate()
     {
-        myRigidBody.linearVelocity = transform.rotation * movementDirection * movementSpeed;
+        myRigidBody.linearVelocity = transform.rotation * movementDirection * directionMultiplier * movementSpeed;
     }
 
 
@@ -50,18 +69,37 @@ public class SnailScript : MonoBehaviour
         if (collision.collider.CompareTag("Floor"))
         {
             transform.rotation = Quaternion.identity; // tzn. 0,0,0
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 1f;
+            isOnCeiling = false;
+            isOnWall = false;
         }
         else if (collision.collider.CompareTag("Left"))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0f;
+            isOnCeiling = false;
+            isOnWall = true;
         }
         else if (collision.collider.CompareTag("Right"))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0f;
+            isOnCeiling = false;
+            isOnWall = true;
         }
         else if (collision.collider.CompareTag("Ceiling"))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0f;
+            isOnCeiling = true;
+            isOnWall = false;
         }
+        
+        directionMultiplier = isOnCeiling ? -1f : 1f;
+
     }
 }
