824df30d233a252db2645c077526bf05e36b3bc6
kzvolska <zvolskam@centrum.cz>
2025-05-18 18:15:57 +0200

    popisky v kodu

--- Snail/Assets/Scripts/SnailScript.cs
+++ Snail/Assets/Scripts/SnailScript.cs
@@ -28,19 +28,13 @@ public class SnailScript : MonoBehaviour
         float inputY = UnityEngine.Input.GetAxisRaw("Vertical");
         horizontalMove = UnityEngine.Input.GetAxisRaw("Horizontal") * movementSpeed;
         movementDirection = new Vector2(UnityEngine.Input.GetAxisRaw("Horizontal"), UnityEngine.Input.GetAxisRaw("Vertical"));
-        if (isOnWall)
-        {
-            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-        }
-        else 
-        {
-            animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-        }
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
 
-        if (movementDirection.x > 0)
+        if (movementDirection.x > 0) //otaceni sneka kdyz leze do leva/do prava
         {
-            transform.localScale = new Vector3(directionMultiplier * -1, 1, 1);
+            transform.localScale = new Vector3(directionMultiplier * -1, 1, 1); //nasobim tim direction multiplier aby se otacel spravne i kdyz je na strope
 
         }
         else if (movementDirection.x < 0)
@@ -70,15 +64,15 @@ public class SnailScript : MonoBehaviour
         {
             transform.rotation = Quaternion.identity; // tzn. 0,0,0
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.gravityScale = 1f;
-            isOnCeiling = false;
-            isOnWall = false;
+            rb.gravityScale = 1f; //na zemi ma gravitaci rovnou 1 
+            isOnCeiling = false; //nastaveni, jestli je na zdi nebo na strope nebo normalne na zemi
+            isOnWall = false; //to isOnWall asi zatim neni potreba, nikde jsem to nakonec nepouzila
         }
         else if (collision.collider.CompareTag("Left"))
         {
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
-            Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.gravityScale = 0f;
+            Rigidbody2D rb = GetComponent<Rigidbody2D>(); //uploaduju data rigidbody z te hry
+            rb.gravityScale = 0f; //nastaveni gravitace na 0 - kdyz leze po stenach a strope
             isOnCeiling = false;
             isOnWall = true;
         }
@@ -98,8 +92,7 @@ public class SnailScript : MonoBehaviour
             isOnCeiling = true;
             isOnWall = false;
         }
-        
-        directionMultiplier = isOnCeiling ? -1f : 1f;
 
+        directionMultiplier = isOnCeiling ? -1f : 1f; //pokud je na strope, otoci se nejak ty vektory (z chata), nasobim tim pak ten pohyb a otaceni, aby se i na strope pohyboval realisticky (pretacel se v souladu se smerem)
     }
 }
